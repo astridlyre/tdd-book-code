976b2541ee2378464a572f10432e8a49feff6358
Erin Burton <eb@flat9.dev>
2021-09-11 14:44:13 -0700

    refactor: created Money and Portfolio modules; improved test design

--- js/test/portfolio.js
+++ js/test/portfolio.js
@@ -4,6 +4,13 @@ import Money from "../money.js";
 import Portfolio from "../portfolio.js";
 
 describe("Test Portfolio class", function () {
+  describe("Test Constructor", function () {
+    it("should initialize a portfolio with the proper amounts", function () {
+      const tenEuros = new Money(10, "EUR");
+      assert.deepStrictEqual(tenEuros, new Portfolio(tenEuros).evaluate("EUR"));
+    });
+  });
+
   describe("Test Addition", function () {
     it("should properly add money of the same currency", function () {
       const fifteenDollars = new Money(15, "USD");
